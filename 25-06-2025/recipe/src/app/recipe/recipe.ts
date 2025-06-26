ff6db82eacff0f2cf16bfea254d339df659706a2
Legin-ML <leginml2004@gmail.com>
2025-06-26 18:06:57 +0530

    2606

---
+++ 25-06-2025/recipe/src/app/recipe/recipe.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { RecipeModel } from '../models/recipe';
+
+@Component({
+  selector: 'app-recipe',
+  imports: [],
+  templateUrl: './recipe.html',
+  styleUrl: './recipe.css'
+})
+export class Recipe {
+    @Input() recipe: RecipeModel | null = null;
+}
---
+++ 25-06-2025/recipe/src/app/services/recipe.service.spec.ts
@@ -0,0 +1,40 @@
+import { TestBed } from '@angular/core/testing';
+import {  HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { RecipeService } from './recipe.service';
+
+describe('RecipeService', () => {
+  let service: RecipeService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers: [
+        RecipeService
+      ]
+    });
+
+    service = TestBed.inject(RecipeService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  it('should fetch all recipes', () => {
+    const mockResponse = {
+      recipes: [{ id: 1, name: 'Pasta' }]
+    };
+
+    service.getAllRecipes().subscribe(data => {
+      expect(data).toEqual(mockResponse.recipes);
+    });
+
+    const req = httpMock.expectOne('https://dummyjson.com/recipes');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+    afterEach(() => {
+    httpMock.verify();
+  });
+
+  
+});
